5e0ad55351b77d180b44c47ae752293a63a6367b
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-10 07:59:36 -0400

    my commit

--- linked-list.js
+++ linked-list.js
@@ -8,14 +8,34 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
+
+
   }
 
   addToHead(val) {
     // Your code here
+    const newNode = new LinkedListNode(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+
   }
 
   addToTail(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+    this.length++;
   }
 
   // You can use this function to help debug
